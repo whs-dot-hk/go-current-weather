679bd69685bfe73e16c9fc86dbba307c7bffe3a6
whs <hswongac@gmail.com>
2020-02-03 10:36:14 +0800

    Add mux

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/whs-dot-hk/go-current-weather
 go 1.13
 
 require (
-	github.com/briandowns/openweathermap v0.13.0 // indirect
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/briandowns/openweathermap v0.13.0
+	github.com/gorilla/mux v1.7.3
+	github.com/heroku/x v0.0.20
+	github.com/joho/godotenv v1.3.0
 )
--- main.go
+++ main.go
@@ -3,26 +3,52 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	owm "github.com/briandowns/openweathermap"
+	"github.com/gorilla/mux"
+	_ "github.com/heroku/x/hmetrics/onload"
 	"github.com/joho/godotenv"
 )
 
+var (
+	apiKey string
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w2, err := owm.NewCurrent("C", "EN", apiKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w2.CurrentByName("Hong Kong, HK")
+
+	t := fmt.Sprintf("%.f°C\n", w2.Main.Temp)
+
+	w.Write([]byte(t))
+}
+
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	apiKey := os.Getenv("OWM_API_KEY")
+	apiKey = os.Getenv("OWM_API_KEY")
 
-	w, err := owm.NewCurrent("C", "EN", apiKey)
-	if err != nil {
-		log.Fatal(err)
+	r := mux.NewRouter()
+	r.HandleFunc("/", Handler)
+
+	if port == "" {
+		port = ":8000"
+	} else {
+		port = fmt.Sprintf(":%s", port)
 	}
 
-	w.CurrentByName("Hong Kong, HK")
-
-	fmt.Printf("%.f°C\n", w.Main.Temp)
+	log.Fatal(http.ListenAndServe(port, r))
 }
